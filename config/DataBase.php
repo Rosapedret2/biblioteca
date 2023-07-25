fb0569c002e488e68c1cfe0b2317985136948f4e
Rosapedret2 <rosapedret2@gmail.com>
2023-07-25 16:05:23 +0200

    index.php y head

---
+++ config/DataBase.php
@@ -0,0 +1,19 @@
+<?php
+class dataBase {
+    private $host = "localhost";
+    private $dbname = "Books_library";
+    private $user = "root";
+    private $password = "";
+    public function conection(){
+        try{
+            $PDO = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
+            return $PDO;
+        }catch(PDOExeption $e){
+            return $e->getMessage();
+        }
+    }
+}
+$object = new dataBase();
+print_r($object->conection());
+
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    require_once("c://xampp/htdocs/biblioteca/view/head/header.php");
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+    require_once("c://xampp/htdocs/biblioteca/view/head/footer.php");
+?>
